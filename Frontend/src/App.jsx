3baf8b8a40dd8a13dd5dadf65ec5e9859c4c8bd5
Kartikmadasanal <kartikmadasanal03@gmail.com>
2024-06-28 20:34:37 +0530

    create routes and pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,28 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Aboutus from "./Pages/Aboutus"
+import Login from "./Pages/Login"
+import Signin from "./Pages/Signin"
+import Contact from "./Pages/Contact"
+import Profilepage from "./Pages/Profilepage"
+import Createpost from "./Pages/Createpost"
+import Navbar from "./Components/Navbar"
+
 
 export default function App() {
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Login" element={<Login/> }/>
+        <Route path="/Signin" element={<Signin/> }/>
+        <Route path="/aboutus" element={<Aboutus/> }/>
+        <Route path="/contact" element={<Contact/> }/>
+        <Route path="/profilepage" element={<Profilepage/> }/>
+        <Route path="/createpost" element={<Createpost/> }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ Frontend/src/Pages/Createpost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Createpost() {
+  return (
+    <div>
+      Createpost
+    </div>
+  )
+}
+
+export default Createpost
