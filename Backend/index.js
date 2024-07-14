3937a734f42405fcf3aafe01a324674902eebac4
Kartikmadasanal <kartikmadasanal03@gmail.com>
2024-07-14 16:29:37 +0530

    final push

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.DATABASE).then(()=>{
 console.log(err);
 })
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000 ;
 
 app.use(express.json())
 app.use(cors({origin:"http://localhost:5173",credentials:true}))
